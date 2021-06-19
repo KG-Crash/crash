9aac46bebf847b5518790f57e2e28eb20d3d058c
boyism80 <boyism80@gmail.com>
2021-06-19 13:32:47 +0900

    젠킨스 더미 스테이지

--- server/Jenkinsfile.groovy
+++ server/Jenkinsfile.groovy
@@ -1,6 +1,10 @@
 pipeline {
     agent { label 'slave' }
     stages {
-        
+        stage('Preperence') {
+            steps{ 
+                    sh 'fab -f deploy/fabfile.py environment:e=development'
+                }
+        }
     }
 }
