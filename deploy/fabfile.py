8af804bbf3738535fd5954b98ea617db652f56a0
boyism80 <boyism80@gmail.com>
2021-06-21 03:05:09 +0900

    #6 테스트 코드 수정

--- deploy/fabfile.py
+++ deploy/fabfile.py
@@ -96,7 +96,7 @@ def deploy(service='game'):
         files.upload_template('appsettings.txt', f'{root}/{appsettings_fname}', context=config, use_jinja=True, template_dir='deploy/templates', use_sudo=True, backup=False)
 
         container_name = f'crash.{service}.{i}'
-        print(f"docker run -it -d --restart unless-stopped --name {container_name} -p {config['own']['port']}:{config['own']['port']} -v {root}/{appsettings_fname}:/app/{appsettings_fname} -e CRASH_ENVIRONMENT={ENVIRONMENT} {image_name}")
+        sudo(f"docker run -it -d --restart unless-stopped --name {container_name} -p {config['own']['port']}:{config['own']['port']} -v {root}/{appsettings_fname}:/app/{appsettings_fname} -e CRASH_ENVIRONMENT={ENVIRONMENT} {image_name}")
 
 @task
 @roles('haproxy.master')
