38a48c1a04e4568fe005cea0525e4542ade98077
suhyeokkim <hermaeusmorarb@gmail.com>
2021-06-28 00:13:13 +0900

    #12 CrashMath 에 계산 코드 옮김

---
+++ client/unity/Assets/Scripts/Game/CrashMath.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using Game;
+using UnityEngine;
+
+public class CrashMath
+{
+    public static Plane GetCreatePlaneFromQuad(Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3, bool reverse)
+    {
+        Vector3[] tris = new Vector3[4];
+        tris[0] = reverse ? p3 : p0;
+        tris[1] = reverse ? p2 : p1;
+        tris[2] = reverse ? p1 : p2;
+        tris[3] = reverse ? p0 : p3;
+        Plane plane;
+        GeometryUtility.TryCreatePlaneFromPolygon(tris, out plane);
+        return plane;
+    }
+
+    public static void GetFrustumPlanes(Camera camera, Rect rectSS, Plane[] frustumPlanes)
+    {
+        var frustumPoints = new Vector3[8];
+        var rectCS = new Rect(
+            new Vector2(rectSS.center.x / camera.pixelWidth, rectSS.center.y / camera.pixelHeight),
+            new Vector2(rectSS.width / camera.pixelWidth, rectSS.height / camera.pixelHeight)
+        );
+            
+        camera.CalculateFrustumCorners(rectCS, camera.farClipPlane, Camera.MonoOrStereoscopicEye.Mono, frustumPoints);
+        Array.Copy(frustumPoints, 0, frustumPoints, 4, 4);
+        camera.CalculateFrustumCorners(rectCS, camera.nearClipPlane, Camera.MonoOrStereoscopicEye.Mono, frustumPoints);
+        for (int i = 0; i < frustumPoints.Length; i++)
+            frustumPoints[i] = camera.transform.TransformPoint(frustumPoints[i]);
+            
+        frustumPlanes[0] = GetCreatePlaneFromQuad(frustumPoints[0], frustumPoints[1], frustumPoints[5], frustumPoints[4], false);
+        frustumPlanes[1] = GetCreatePlaneFromQuad(frustumPoints[2], frustumPoints[3], frustumPoints[7], frustumPoints[6], false);
+        frustumPlanes[2] = GetCreatePlaneFromQuad(frustumPoints[0], frustumPoints[3], frustumPoints[7], frustumPoints[4], true);
+        frustumPlanes[3] = GetCreatePlaneFromQuad(frustumPoints[1], frustumPoints[2], frustumPoints[6], frustumPoints[5], false);
+        frustumPlanes[4] = GetCreatePlaneFromQuad(frustumPoints[0], frustumPoints[1], frustumPoints[2], frustumPoints[3], true);
+        frustumPlanes[5] = GetCreatePlaneFromQuad(frustumPoints[4], frustumPoints[5], frustumPoints[6], frustumPoints[7], false);
+    }
+
+    public static Vector3Int EncodePosition(Vector3 position)
+    {
+        return new Vector3Int(Mathf.FloorToInt(position.x * 1000.0f), Mathf.FloorToInt(position.y * 1000.0f),Mathf.FloorToInt(position.z * 1000.0f));
+    }
+    public static Vector3 DecodePosition(Vector3Int position)
+    {
+        return new Vector3(position.x / 1000.0f, position.y / 1000.0f, position.z / 1000.0f);
+    }
+}
--- client/unity/Assets/Scripts/Game/GameController.cs
+++ client/unity/Assets/Scripts/Game/GameController.cs
@@ -50,40 +50,6 @@ namespace Game
             
         }
 
-        public static Plane GetCreatePlaneFromQuad(Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3, bool reverse)
-        {
-            Vector3[] tris = new Vector3[4];
-            tris[0] = reverse? p3: p0;
-            tris[1] = reverse? p2: p1;
-            tris[2] = reverse? p1: p2;
-            tris[3] = reverse? p0: p3;
-            Plane plane;
-            GeometryUtility.TryCreatePlaneFromPolygon(tris, out plane);
-            return plane;
-        }
-
-        public void GetFrustumPlanes(Camera camera, Rect rectSS, Plane[] frustumPlanes)
-        {
-            var frustumPoints = new Vector3[8];
-            var rectCS = new Rect(
-            new Vector2(rectSS.center.x / camera.pixelWidth, rectSS.center.y / camera.pixelHeight),
-                new Vector2(rectSS.width / camera.pixelWidth, rectSS.height / camera.pixelHeight)
-            );
-            
-            camera.CalculateFrustumCorners(rectCS, camera.farClipPlane, Camera.MonoOrStereoscopicEye.Mono, frustumPoints);
-            Array.Copy(frustumPoints, 0, frustumPoints, 4, 4);
-            camera.CalculateFrustumCorners(rectCS, camera.nearClipPlane, Camera.MonoOrStereoscopicEye.Mono, frustumPoints);
-            for (int i = 0; i < frustumPoints.Length; i++)
-                frustumPoints[i] = camera.transform.TransformPoint(frustumPoints[i]);
-            
-            frustumPlanes[0] = GetCreatePlaneFromQuad(frustumPoints[0], frustumPoints[1], frustumPoints[5], frustumPoints[4], false);
-            frustumPlanes[1] = GetCreatePlaneFromQuad(frustumPoints[2], frustumPoints[3], frustumPoints[7], frustumPoints[6], false);
-            frustumPlanes[2] = GetCreatePlaneFromQuad(frustumPoints[0], frustumPoints[3], frustumPoints[7], frustumPoints[4], true);
-            frustumPlanes[3] = GetCreatePlaneFromQuad(frustumPoints[1], frustumPoints[2], frustumPoints[6], frustumPoints[5], false);
-            frustumPlanes[4] = GetCreatePlaneFromQuad(frustumPoints[0], frustumPoints[1], frustumPoints[2], frustumPoints[3], true);
-            frustumPlanes[5] = GetCreatePlaneFromQuad(frustumPoints[4], frustumPoints[5], frustumPoints[6], frustumPoints[7], false);
-        }
-
         private Vector3[] frustumPoints = new Vector3[8];
         private Plane[] frustumPlanes = new Plane[6]; 
         
@@ -99,7 +65,7 @@ namespace Game
 
             if (rect.size != Vector2.zero)
             {
-                GetFrustumPlanes(selectedCamera, rect, frustumPlanes); 
+                CrashMath.GetFrustumPlanes(selectedCamera, rect, frustumPlanes); 
                 UnityObjects.IntersectUnits(frustumPlanes, units, selectedUnits);
             }
             else
@@ -125,18 +91,6 @@ namespace Game
             _selectedUnits = selectedUnitList;
         }
 
-        public Vector3Int EncodePosition(Vector3 pos)
-        {
-            return new Vector3Int(
-                Mathf.RoundToInt(pos.x * 1000.0f), 
-                Mathf.RoundToInt(pos.y * 1000.0f), 
-                Mathf.RoundToInt(pos.z * 1000.0f));
-        }
-        public Vector3 DecodePosition(Vector3Int netPos)
-        {
-            return new Vector3(netPos.x / 1000.0f, netPos.y / 1000.0f, netPos.z / 1000.0f);
-        }
-        
         public bool MoveSelectedUnitTo(Vector2 positionSS)
         {           
             var uobj = UnityResources._instance.Get("objects");
@@ -147,11 +101,7 @@ namespace Game
             {
                 foreach (var unit in _selectedUnits)
                 {
-                    AppendCommand(
-                        CommandFactory.CreateMoveCommand(
-                            unit.unitID, EncodePosition(hitInfo.point)
-                            )
-                        );
+                    unit.MoveTo(CrashMath.EncodePosition(hitInfo.point));
                 }
 
                 return true;
@@ -174,7 +124,7 @@ namespace Game
                     case CommandType.Move:
                         var unit = player.units[command.behaiveUnitID];
                         var moveCommand = (MoveCommand) command;
-                        unit.MoveTo(DecodePosition(moveCommand._position));
+                        unit.MoveTo(CrashMath.DecodePosition(moveCommand._position));
                         break;
                     case CommandType.AttackSingleTarget:
                         break;
