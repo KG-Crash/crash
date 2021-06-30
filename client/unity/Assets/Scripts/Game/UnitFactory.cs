ced4e5ea4c2729a23da06351629771c9a03fad77
Kim Sungju <tjdwn0802@naver.com>
2021-07-01 03:48:48 +0900

    #15 유닛 생성시 리스너 붙임

--- client/unity/Assets/Scripts/Game/UnitFactory.cs
+++ client/unity/Assets/Scripts/Game/UnitFactory.cs
@@ -6,12 +6,13 @@ namespace Game
     {
         private static uint _unitIDStepper = 0;
         
-        public static Unit GetNewUnit(int unitOriginID, uint teamID, UnitTable unitTable)
+        public static Unit GetNewUnit(int unitOriginID, uint teamID, UnitTable unitTable, IUnit listener)
         {
             var unitOrigin = unitTable.GetOrigin(unitOriginID);
             var unit = Object.Instantiate(unitOrigin);
             unit.unitID = _unitIDStepper++;
             unit.teamID = teamID;
+            unit.listener = listener;
             return unit;
         }
     }
