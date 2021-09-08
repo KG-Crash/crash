46cf5a9aa95f1c9f719d518ce233ae8ba43e75b8
Kim Sungju <tjdwn0802@naver.com>
2021-09-08 18:28:41 +0900

    #47 투사체 이동, 상태, 리스너 추가

--- client/unity/Assets/Scripts/Game/Projectile.cs
+++ client/unity/Assets/Scripts/Game/Projectile.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Shared;
+using Shared.Table;
 using System;
 using FixMath.NET;
 
@@ -9,72 +10,160 @@ namespace Game
 {
     public class Projectile : MonoBehaviour
     {
-        [SerializeField] private int _projectileOriginID;
-        [SerializeField] public Animator _animator;
-        [SerializeField] private Rigidbody _rigidbody;
+        public interface Listener
+        {
+            void OnProjectileReach(Unit owner, Projectile projectile);
+        }
 
-        [SerializeField] private Bounds _bounds = new Bounds();
-        [SerializeField] private Renderer[] _renderers;
+        [SerializeField] private int _projectileOriginID; 
+        [SerializeField] private uint _projectileID;
 
-        [SerializeField] private uint _ownerUnitID;
-        [NonSerialized] private ProjectileState _currentState;
+        //[SerializeField] public Animator _animator;
+        //[SerializeField] private Rigidbody _rigidbody;
+        //[SerializeField] private Bounds _bounds = new Bounds();
+        //[SerializeField] private Renderer[] _renderers;
 
-        [NonSerialized] private FixVector3? _origin;
-        [NonSerialized] private FixVector3? _target;
+        [SerializeField] private ProjectileState _currentState;
+        [NonSerialized] private FixVector3? _moveTarget;
+
+        [NonSerialized] private Unit? _target;
+        [SerializeField] private Unit? _owner;
+
+        [NonSerialized] private Listener _listener;
+
+        public FixVector3 position { get; set; }
 
 
-        public Shared.Table.Projectile _table { get; private set; }
+        public Shared.Table.Projectile table { get; private set; }
 
         public int projectileOriginID
         {
             get => _projectileOriginID;
         }
+        public uint projectileID
+        {
+            get => _projectileID;
+            set => _projectileID = value;
+        }
         public Fix64 speed
         {
             get
             {
-                var speed = _table.Speed;
+                var speed = table.Speed;
                 return (Fix64)speed;
             }
         }
 
-        public bool targeting
+        public bool type
         {
             get
             {
-                var targeting = _table.Targeting;
-                return targeting;
+                var type = table.Targeting;
+                return type;
             }
         }
 
-        public uint ownerUnitID
+        public Unit owner
+        {
+            get => _owner;
+            set => _owner = value;
+        }
+
+        public Unit target
+        {
+            get => _target;
+            set => _target = value;
+        }
+
+        public FixVector3? moveTarget
         {
             get
             {
-                return _ownerUnitID;
+                if (this.type.Equals(true))
+                    return _target?.position ?? _moveTarget;
+                else
+                    return _moveTarget;
             }
-            set
-            {
-                _ownerUnitID = value;
-            }
+            set => _moveTarget = value;
+        }
+        public Listener listener
+        {
+            get => _listener;
+            set => _listener = value;
         }
 
 
         private void Awake()
         {
-            
-        }
-
-        // Start is called before the first frame update
-        private void Start()
-        {
-
+            this.table = Table.From<TableProjectile>()[this._projectileOriginID];
+            _currentState = ProjectileState.Shoot;
         }
 
         // Update is called once per frame
         private void Update()
         {
+            Action(); 
+
+            this.transform.position = this.position;
+        }
+
+        private void Action()
+        {
+            switch (_currentState)
+            {
+                case ProjectileState.Disable:
+                    break;
+                case ProjectileState.Shoot:
+                    break;
+
+                case ProjectileState.Move:
+                    DeltaMove((Fix64)Time.deltaTime);
+                    break;
+
+                case ProjectileState.Hit:
+                    listener?.OnProjectileReach(owner, this);
+                    break;
+            }
+        }
+        public void Disable()
+        {
+            _currentState = ProjectileState.Disable;
+            target = null;
+            owner = null;
+            moveTarget = null;
 
         }
+        public void Shoot()
+        {
+            moveTarget = target.position;
+            _currentState = ProjectileState.Move;            
+        }
+        public void Hit()
+        {
+            _currentState = ProjectileState.Hit;
+        }
+
+        private void DeltaMove(Fix64 delta)
+        {
+            if (moveTarget == null)
+                return;
+
+            var diff = (moveTarget.Value - position);
+            var magnitude = diff.magnitude;
+
+            if (magnitude != Fix64.Zero)
+            {
+                var direction = diff / magnitude;
+                transform.LookAt(moveTarget.Value);
+
+                var arrived = magnitude < (speed * delta);
+                if (arrived)
+                    Hit();
+                else
+                    position += (direction * speed * delta);
+            }
+            else
+                Hit();
+        }        
     }
 }
