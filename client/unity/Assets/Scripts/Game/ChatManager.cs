c35b783aad623fea81cacd6ba75e32f9a558c3c8
Kim Sungju <tjdwn0802@naver.com>
2021-09-15 18:46:27 +0900

    #58 채팅 메시지 파싱
    파싱한 데이터로 해당 함수 리플렉션으로 호출

--- client/unity/Assets/Scripts/Game/ChatManager.cs
+++ client/unity/Assets/Scripts/Game/ChatManager.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using System;
+using System.Text.RegularExpressions;
+using System.Reflection;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -12,6 +15,7 @@ namespace Game
         [SerializeField] private InputField _input;
         [SerializeField] private ScrollRect _scrollRect;
         [SerializeField] private string _name;
+
         // Start is called before the first frame update
         void Start()
         {
@@ -30,16 +34,114 @@ namespace Game
         {
             if (_input.text.Equals(" ") || _input.text.Equals(""))
                 return;
+
             string msg = _input.text;
-            RecvMessage(msg);
+            string resultMsg = "";
+
             _input.ActivateInputField();
             _input.text = "";
+
+            resultMsg = ParseMessage(msg);
+            if(msg.Equals(resultMsg))
+                RecvMessage(msg);
+            else               
+                Debug.Log(resultMsg);
+            return;            
         }
 
         public void RecvMessage(string msg)
         {
             _chatLog.text += "\n" + _name + " : " + msg;
             _scrollRect.verticalNormalizedPosition = 0.0f;
+        }
+
+        public string ParseMessage(string msg)
+		{
+            MethodInfo methodInfo = null;
+            List<int> parameter = new List<int>();
+
+            foreach (var method in GetCommandMethodInfos())
+			{
+                Match m = Regex.Match(msg, method.GetCustomAttribute<BuildCommandAttribute>().command);
+                if (m.Success)
+                    methodInfo = method;
+            }
+
+            if (methodInfo == null)
+                return msg;
+
+
+            string pattern = @"\d";
+
+            MatchCollection matches = Regex.Matches(msg, pattern);
+            if (matches.Count <= 0)
+                return msg;
+            
+
+            foreach(var m in matches)
+			{
+                parameter.Add(int.Parse(m.ToString()));
+			}
+
+            methodInfo.Invoke(null , new object[] { parameter.ToArray() });
+            msg = methodInfo.GetCustomAttribute<BuildCommandAttribute>().command + "Chit enable";
+            return msg;
+        }
+
+        public List<string> GetCommandList()
+		{
+            List<string> commandList = new List<string>();
+
+            var methods = typeof(ChitSet).GetMethods().Where(x =>
+            {
+                if (x.GetCustomAttribute<BuildCommandAttribute>() == null)
+                    return false;
+                if (x.ReturnType != typeof(void))
+                    return false;
+
+                var parameters = x.GetParameters();
+                if (parameters.Length != 1)
+                    return false;
+
+                if (parameters[0].ParameterType.GetTypeInfo() != typeof(int[]))
+                    return false;
+
+                return true;
+
+            });
+            
+            foreach (var method in methods)
+            {
+                var a = method.GetCustomAttribute<BuildCommandAttribute>();
+                commandList.Add(a.command);
+            }
+
+            return commandList;
+		}
+
+        public List<MethodInfo> GetCommandMethodInfos()
+        {
+            List<MethodInfo> methodList = new List<MethodInfo>();
+
+            var methods = typeof(ChitSet).GetMethods().Where(x =>
+            {
+                if (x.GetCustomAttribute<BuildCommandAttribute>() == null)
+                    return false;
+                if (x.ReturnType != typeof(void))
+                    return false;
+
+                var parameters = x.GetParameters();
+                if (parameters.Length != 1)
+                    return false;
+
+                if (parameters[0].ParameterType.GetTypeInfo() != typeof(int[]))
+                    return false;
+
+                return true;
+
+            });
+
+            return methods.ToList();
         }
     }
 }
