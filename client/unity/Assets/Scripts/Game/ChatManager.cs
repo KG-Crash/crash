5a77264883ac7433f649b8667cc2d0a860eca8f1
Kim Sungju <tjdwn0802@naver.com>
2021-10-13 01:27:40 +0900

    #58 메시지 파싱 Regex 수정
    정규식 그룹을 이용하여 수정

--- client/unity/Assets/Scripts/Game/ChatManager.cs
+++ client/unity/Assets/Scripts/Game/ChatManager.cs
@@ -9,139 +9,150 @@ using UnityEngine.UI;
 
 namespace Game
 {
-    public class ChatManager : MonoBehaviour
-    {
-        [SerializeField] private Text _chatLog;
-        [SerializeField] private InputField _input;
-        [SerializeField] private ScrollRect _scrollRect;
-        [SerializeField] private string _name;
+	public class ChatManager : MonoBehaviour
+	{
+		[SerializeField] private Text _chatLog;
+		[SerializeField] private InputField _input;
+		[SerializeField] private ScrollRect _scrollRect;
+		[SerializeField] private string _name;
 
-        // Start is called before the first frame update
-        void Start()
-        {
-            _scrollRect.verticalNormalizedPosition = 0.0f;
-            _name = "ME";
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-            if (Input.GetKeyDown(KeyCode.Return))
-                SendMessage();
-        }
-
-        public void SendMessage()
-        {
-            if (_input.text.Equals(" ") || _input.text.Equals(""))
-                return;
-
-            string msg = _input.text;
-            string resultMsg = "";
-
-            _input.ActivateInputField();
-            _input.text = "";
-
-            resultMsg = ParseMessage(msg);
-            if(msg.Equals(resultMsg))
-                RecvMessage(msg);
-            else               
-                Debug.Log(resultMsg);
-            return;            
-        }
-
-        public void RecvMessage(string msg)
-        {
-            _chatLog.text += "\n" + _name + " : " + msg;
-            _scrollRect.verticalNormalizedPosition = 0.0f;
-        }
-
-        public string ParseMessage(string msg)
+		// Start is called before the first frame update
+		void Start()
 		{
-            MethodInfo methodInfo = null;
-            List<int> parameter = new List<int>();
-
-            foreach (var method in GetCommandMethodInfos())
-			{
-                Match m = Regex.Match(msg, method.GetCustomAttribute<BuildCommandAttribute>().command);
-                if (m.Success)
-                    methodInfo = method;
-            }
-
-            if (methodInfo == null)
-                return msg;
-
-
-            string pattern = @"\d";
-
-            MatchCollection matches = Regex.Matches(msg, pattern);
-            if (matches.Count <= 0)
-                return msg;
-            
-
-            foreach(var m in matches)
-			{
-                parameter.Add(int.Parse(m.ToString()));
-			}
-
-            methodInfo.Invoke(null , new object[] { parameter.ToArray() });
-            msg = methodInfo.GetCustomAttribute<BuildCommandAttribute>().command + "Chit enable";
-            return msg;
-        }
-
-        public List<string> GetCommandList()
-		{
-            List<string> commandList = new List<string>();
-
-            var methods = typeof(GameController).GetMethods().Where(x =>
-            {
-                if (x.GetCustomAttribute<BuildCommandAttribute>() == null)
-                    return false;
-                if (x.ReturnType != typeof(void))
-                    return false;
-
-                var parameters = x.GetParameters();
-                if (parameters.Length != 1)
-                    return false;
-
-                if (parameters[0].ParameterType.GetTypeInfo() != typeof(int[]))
-                    return false;
-
-                return true;
-
-            });
-            
-            foreach (var method in methods)
-            {
-                var a = method.GetCustomAttribute<BuildCommandAttribute>();
-                commandList.Add(a.command);
-            }
-
-            return commandList;
+			_scrollRect.verticalNormalizedPosition = 0.0f;
+			_name = "ME";
 		}
 
-        public List<MethodInfo> GetCommandMethodInfos()
-        {
-            List<MethodInfo> methodList = new List<MethodInfo>();
+		// Update is called once per frame
+		void Update()
+		{
+			if (Input.GetKeyDown(KeyCode.Return))
+				SendMessage();
+		}
 
-            var methods = typeof(GameController).GetMethods().Where(x =>
-            {
-                if (x.GetCustomAttribute<BuildCommandAttribute>() == null)
-                    return false;
-                if (x.ReturnType != typeof(void))
-                    return false;
+		public void SendMessage()
+		{
+			if (_input.text.Equals(" ") || _input.text.Equals(""))
+				return;
 
-                var parameters = x.GetParameters();
-                if (parameters.Length != 1)
-                    return false;
+			string msg = _input.text;
+			string resultMsg = "";
 
-                if (parameters[0].ParameterType.GetTypeInfo() != typeof(int[]))
-                    return false;
+			_input.ActivateInputField();
+			_input.text = "";
 
-                return true;
+			resultMsg = ParseMessage(msg);
 
-            });
+			if (msg.Equals(resultMsg))
+				RecvMessage(resultMsg);
+			else
+			{
+				Debug.Log(resultMsg);
+				PrintLog(resultMsg);
+			}
+			return;
+		}
 
-            return methods.ToList();
-        }
-    }
+		public void RecvMessage(string msg)
+		{
+			_chatLog.text += "\n" + _name + " : " + msg;
+			_scrollRect.verticalNormalizedPosition = 0.0f;
+		}
+
+		public void PrintLog(string msg)
+		{
+			_chatLog.text += "\n" + msg;
+			_scrollRect.verticalNormalizedPosition = 0.0f;
+		}
+
+
+		public string ParseMessage(string msg)
+		{
+			MethodInfo methodInfo = null;
+			List<int> parameter = new List<int>();
+
+			foreach (var method in GetCommandMethodInfos())
+			{
+				String command = method.GetCustomAttribute<BuildCommandAttribute>().command;
+
+				Regex regex = new Regex("(?<command>" + command + ")" + @"\s*(?<param>.+\d)\s*");
+				Match match = regex.Match(msg);
+
+				if (!match.Groups["command"].ToString().Equals("")) // 해당 커맨드가 있으면
+				{
+					methodInfo = method;
+					foreach (var param in match.Groups["param"].ToString().Split())
+					{
+						parameter.Add(int.Parse(param));
+					}
+				}
+			}
+
+			if (methodInfo == null)
+				return msg;
+
+			if (parameter.Count < 1)
+				return msg;
+
+			methodInfo.Invoke(null, new object[] { parameter.ToArray() });
+			msg = methodInfo.GetCustomAttribute<BuildCommandAttribute>().command + " Chit enable";
+			return msg;
+		}
+
+		public List<string> GetCommandList()
+		{
+			List<string> commandList = new List<string>();
+
+			var methods = typeof(GameController).GetMethods().Where(x =>
+			{
+				if (x.GetCustomAttribute<BuildCommandAttribute>() == null)
+					return false;
+				if (x.ReturnType != typeof(void))
+					return false;
+
+				var parameters = x.GetParameters();
+				if (parameters.Length != 1)
+					return false;
+
+				if (parameters[0].ParameterType.GetTypeInfo() != typeof(int[]))
+					return false;
+
+				return true;
+
+			});
+
+			foreach (var method in methods)
+			{
+				var a = method.GetCustomAttribute<BuildCommandAttribute>();
+				commandList.Add(a.command);
+			}
+
+			return commandList;
+		}
+
+		public List<MethodInfo> GetCommandMethodInfos()
+		{
+			List<MethodInfo> methodList = new List<MethodInfo>();
+
+			var methods = typeof(GameController).GetMethods().Where(x =>
+			{
+				if (x.GetCustomAttribute<BuildCommandAttribute>() == null)
+					return false;
+				if (x.ReturnType != typeof(void))
+					return false;
+
+				var parameters = x.GetParameters();
+				if (parameters.Length != 1)
+					return false;
+
+				if (parameters[0].ParameterType.GetTypeInfo() != typeof(int[]))
+					return false;
+
+				return true;
+
+			});
+
+			return methods.ToList();
+		}
+	}
 }
