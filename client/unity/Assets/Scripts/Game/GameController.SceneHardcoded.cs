edfcf266b955384bd976750da87de5b6c47cf2f2
suhyeokkim <hermaeusmorarb@gmail.com>
2021-10-28 17:34:39 +0900

    #82 씬 유닛 생성 로직 SceneHardcoded 로 이동

--- client/unity/Assets/Scripts/Game/GameController.SceneHardcoded.cs
+++ client/unity/Assets/Scripts/Game/GameController.SceneHardcoded.cs
@@ -6,5 +6,56 @@ namespace Game
 {
     public partial class GameController
     {
+        public Unit SpawnUnitToPlayerStart(int spawnUnitOriginID, Player ownPlayer)
+        {
+            var newUnit = _unitFactory.GetNewUnit(spawnUnitOriginID, _unitPrefabTable, _map, ownPlayer, this);
+            ownPlayer.units.Add(newUnit);
+            newUnit.position = GetSpawnPosition(ownPlayer.spawnIndex);
+
+            return newUnit;
+        }
+
+        public Unit SpawnUnitToPosition(int spawnUnitOriginID, Player ownPlayer, FixVector3 position)
+        {
+            var newUnit = _unitFactory.GetNewUnit(spawnUnitOriginID, _unitPrefabTable, _map, ownPlayer, this);
+            ownPlayer.units.Add(newUnit);
+            newUnit.position = position;
+
+            return newUnit;
+        }
+
+        public Unit SpawnUnitToPosition(int spawnUnitOriginID, uint playerID, FixVector3 position)
+        {
+            return SpawnUnitToPosition(spawnUnitOriginID, GetPlayer(playerID), position);
+        }
+
+        /*
+         * 유닛 스폰 및 해당 맵의 첫번째 처리 루틴,
+         * 지금은 임시 코드가 들어감.
+         */
+        private void OnLoadScene()
+        {   
+            _allPlayerByTeam = new Team();
+            
+            var player = AddNewPlayer(0, 0);
+            
+            SpawnUnitToPlayerStart(3, player);
+            //SpawnUnitToPlayerStart(1, player);
+            //SpawnUnitToPlayerStart(2, player);
+
+            var otherPlayer = AddNewPlayer(1, 1);
+            
+            SpawnUnitToPlayerStart(0, otherPlayer);
+            SpawnUnitToPlayerStart(1, otherPlayer);
+
+            _player = player;
+
+            if (_player.units.Any())
+            {
+                var sum = _player.units.Aggregate(FixVector3.Zero, (acc, x) => acc + x.position);
+                var lookPosition = sum / _player.units.Count();
+                _focusTransform.position = lookPosition;
+            }
+        }
     }
 }
--- client/unity/Assets/Scripts/Game/GameController.cs
+++ client/unity/Assets/Scripts/Game/GameController.cs
@@ -59,63 +59,12 @@ namespace Game
             return newPlayer;
         }
 
-        public Unit SpawnUnitToPlayerStart(int spawnUnitOriginID, Player ownPlayer)
-        {
-            var newUnit = _unitFactory.GetNewUnit(spawnUnitOriginID, _unitPrefabTable, _map, ownPlayer, this);
-            ownPlayer.units.Add(newUnit);
-            newUnit.position = GetSpawnPosition(ownPlayer.spawnIndex);
-
-            return newUnit;
-        }
-
-        public Unit SpawnUnitToPosition(int spawnUnitOriginID, Player ownPlayer, FixVector3 position)
-        {
-            var newUnit = _unitFactory.GetNewUnit(spawnUnitOriginID, _unitPrefabTable, _map, ownPlayer, this);
-            ownPlayer.units.Add(newUnit);
-            newUnit.position = position;
-
-            return newUnit;
-        }
-
-        public Unit SpawnUnitToPosition(int spawnUnitOriginID, uint playerID, FixVector3 position)
-        {
-            return SpawnUnitToPosition(spawnUnitOriginID, GetPlayer(playerID), position);
-        }
-
-        /*
-         * 유닛 스폰 및 해당 맵의 첫번째 처리 루틴,
-         * 지금은 임시 코드가 들어감.
-         */
-        private void OnLoadScene()
-        {   
-            _allPlayerByTeam = new Team();
-            
-            var player = AddNewPlayer(0, 0);
-            
-            SpawnUnitToPlayerStart(3, player);
-            //SpawnUnitToPlayerStart(1, player);
-            //SpawnUnitToPlayerStart(2, player);
-
-            var otherPlayer = AddNewPlayer(1, 1);
-            
-            SpawnUnitToPlayerStart(0, otherPlayer);
-            SpawnUnitToPlayerStart(1, otherPlayer);
-
-            _player = player;
-
-            if (_player.units.Any())
-            {
-                var sum = _player.units.Aggregate(FixVector3.Zero, (acc, x) => acc + x.position);
-                var lookPosition = sum / _player.units.Count();
-                _focusTransform.position = lookPosition;
-            }
-        }
-        
         private void Awake()
         {
             Handler.Bind(this, Dispatcher.Instance);
             
             OnLoadScene();
+            
             _projectilePool = new ProjectilePool(_projectilehPrefabTable, 15, this, _poolOffset);
             _selectedUnits = new List<Unit>();
             _allUnitInFrustum = new List<Unit>();
@@ -131,12 +80,12 @@ namespace Game
             UpdateForDebug();
             _player.UpdateUpgrade(UnityEngine.Time.time);
             UpdateUnitInFrustumPlane();
-
-            if (Frame++ >= TPS)
-            {
-                OnTurnChanged();
-                Frame = 0;
-            }
+            //
+            // if (Frame++ >= TPS)
+            // {
+            //     OnTurnChanged();
+            //     Frame = 0;
+            // }
         }
 
         private void OnDestroy()
