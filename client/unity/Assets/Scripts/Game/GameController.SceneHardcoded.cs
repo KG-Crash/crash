9a2643e15158e3c8f7870796078ee263a1992157
suhyeokkim <hermaeusmorarb@gmail.com>
2021-10-28 17:35:15 +0900

    #82 플레이어 3:3, 각자 상대에게 어택땅 디폴트 설정

--- client/unity/Assets/Scripts/Game/GameController.SceneHardcoded.cs
+++ client/unity/Assets/Scripts/Game/GameController.SceneHardcoded.cs
@@ -28,7 +28,9 @@ namespace Game
         {
             return SpawnUnitToPosition(spawnUnitOriginID, GetPlayer(playerID), position);
         }
-
+        
+        #region 유닛 스폰 하드코드
+        
         /*
          * 유닛 스폰 및 해당 맵의 첫번째 처리 루틴,
          * 지금은 임시 코드가 들어감.
@@ -37,17 +39,39 @@ namespace Game
         {   
             _allPlayerByTeam = new Team();
             
-            var player = AddNewPlayer(0, 0);
+            var player1 = AddPlayerAndUnit(true, 0, 3, 1, 2);
+            var player2 = AddPlayerAndUnit(true, 1, 3, 1, 2);
+            var player3 = AddPlayerAndUnit(true, 2, 3, 1, 2);
             
-            SpawnUnitToPlayerStart(3, player);
-            //SpawnUnitToPlayerStart(1, player);
-            //SpawnUnitToPlayerStart(2, player);
+            var player4 = AddPlayerAndUnit(false, 3, 3, 1, 2);
+            var player5 = AddPlayerAndUnit(false, 4, 3, 1, 2);
+            var player6 = AddPlayerAndUnit(false, 5, 3, 1, 2);
 
-            var otherPlayer = AddNewPlayer(1, 1);
+            player1.targetPlayerID = player4.playerID;
+            player2.targetPlayerID = player5.playerID;
+            player3.targetPlayerID = player6.playerID;
             
-            SpawnUnitToPlayerStart(0, otherPlayer);
-            SpawnUnitToPlayerStart(1, otherPlayer);
+            player4.targetPlayerID = player1.playerID;
+            player5.targetPlayerID = player2.playerID;
+            player6.targetPlayerID = player3.playerID;
+                        
+            MoveCameraToUnitPos(player1);
+        }
 
+        private Player AddPlayerAndUnit(bool home, int playerID, params int[] unitTypes)
+        {
+            var player = AddNewPlayer(home? (uint)0: 1, playerID);
+
+            foreach (var unitType in unitTypes)
+            {
+                SpawnUnitToPlayerStart(unitType, player);
+            }
+            
+            return player;
+        }
+
+        private void MoveCameraToUnitPos(Player player)
+        {
             _player = player;
 
             if (_player.units.Any())
@@ -57,5 +81,7 @@ namespace Game
                 _focusTransform.position = lookPosition;
             }
         }
+        
+        #endregion 유닛 스폰 하드코드
     }
 }
