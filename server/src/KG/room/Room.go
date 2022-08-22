6d0c96f645decc4c95248b31e3a573a45cc1cc36
채승현 <boyism80@gmail.com>
2022-08-23 00:41:27 +0900

    #157 게임룸 입장

--- server/src/KG/room/Room.go
+++ server/src/KG/room/Room.go
@@ -3,6 +3,7 @@ package room
 import (
 	"KG/session"
 	"crypto/rand"
+	"errors"
 	"math"
 	"math/big"
 
@@ -15,39 +16,73 @@ type Config struct {
 }
 
 type Room struct {
-	id           string
-	users        map[int][]*session.Session
-	playing      bool
-	config       Config
-	master       *session.Session
-	seed         int64
-	nextSequence int32
-	mapSequence  map[int32]*session.Session
+	Id      string
+	Users   map[int][]*session.Session
+	Playing bool
+	Config  Config
+	Master  *session.Session
+	Seed    int64
 }
 
 func (room *Room) ID() string {
-	return room.id
+	return room.Id
 }
 
 func (room *Room) Title() string {
-	return room.config.Title
+	return room.Config.Title
 }
 
-func (room *Room) emptyTeamID() (int, error) {
+func (room *Room) nextTeamSequence() (int, error) {
+	min := 0xFFFFFFFF
+	result := -1
+	for team, max := range room.Config.Team {
+		users, ok := room.Users[team]
+		count := 0
+		if ok {
+			count = len(users)
+		}
 
-	return 0, nil
+		isFull := int(max) <= count
+		if isFull {
+			continue
+		}
+
+		if count >= min {
+			continue
+		}
+
+		result = team
+	}
+
+	if result == -1 {
+		return result, errors.New("full")
+	}
+
+	return result, nil
+}
+
+func (room *Room) Enter(session *session.Session) error {
+
+	team, err := room.nextTeamSequence()
+	if err != nil {
+		return err
+	}
+
+	room.Users[team] = append(room.Users[team], session)
+	return nil
 }
 
 func New(master *session.Session, config Config) Room {
 	seed, _ := rand.Int(rand.Reader, big.NewInt(math.MaxInt64))
-	return Room{
-		id:           uuid.NewString(),
-		users:        map[int][]*session.Session{},
-		playing:      false,
-		config:       config,
-		master:       master,
-		seed:         seed.Int64(),
-		nextSequence: 0,
-		mapSequence:  map[int32]*session.Session{},
+	room := Room{
+		Id:      uuid.NewString(),
+		Users:   map[int][]*session.Session{},
+		Playing: false,
+		Config:  config,
+		Master:  master,
+		Seed:    seed.Int64(),
 	}
+
+	room.Enter(master)
+	return room
 }
