977980ddd4265ee67e0baee16a8930d569bfa663
suhyeokkim <hermaeusmorarb@gmail.com>
2022-09-11 21:02:04 +0900

    #171 UI 오브젝트엔 RectTransform 필수

--- client/unity/Assets/KG/Runtime/Scripts/UIComponent.cs
+++ client/unity/Assets/KG/Runtime/Scripts/UIComponent.cs
@@ -4,7 +4,18 @@ using UnityEngine.EventSystems;
 
 namespace KG
 {
+    [RequireComponent(typeof(RectTransform))]
     public abstract class UIComponent : UIBehaviour
     {
+        private RectTransform _rectTransform;
+        public RectTransform rectTransform
+        {
+            get
+            {
+                if (_rectTransform == null)
+                    _rectTransform = GetComponent<RectTransform>();
+                return _rectTransform;
+            }
+        }
     }
 }
--- client/unity/Assets/KG/Runtime/Scripts/UIView.cs
+++ client/unity/Assets/KG/Runtime/Scripts/UIView.cs
@@ -4,11 +4,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 namespace KG
 {
-    public abstract class UIView : MonoBehaviour
+    [RequireComponent(typeof(RectTransform))]
+    public abstract class UIView : UIBehaviour
     {
+        private RectTransform _rectTransform;
+        public RectTransform rectTransform
+        {
+            get
+            {
+                if (_rectTransform == null)
+                    _rectTransform = GetComponent<RectTransform>();
+                return _rectTransform;
+            }
+        }
+
         public virtual async Task OnLoad()
         { }
 
