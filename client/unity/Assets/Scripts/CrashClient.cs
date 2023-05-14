b2ddb72e50076f718ed9160e01b933e60db6e23b
suhyeokkim <hermaeusmorarb@gmail.com>
2023-05-14 22:02:48 +0900

    #196 패킷 전송 시 유니티 로그 추가

--- client/unity/Assets/Scripts/CrashClient.cs
+++ client/unity/Assets/Scripts/CrashClient.cs
@@ -1,7 +1,27 @@
+using System;
 using System.Threading.Tasks;
 using Module;
 using Network;
 using Protocol;
+using UnityEngine;
+
+public struct LogScope : IDisposable
+{
+    private string prefix;
+
+    public LogScope(string prefix)
+    {
+        this.prefix = prefix; 
+        if (CrashResources.LoadServerSettings().logSendRequest) 
+            Debug.Log($"{prefix}, Start");
+    }
+
+    public void Dispose()
+    {
+        if (CrashResources.LoadServerSettings().logSendRequest)
+            Debug.Log($"{prefix}, End");  
+    } 
+}
 
 public class CrashClient : BaseClient
 {
@@ -10,6 +30,19 @@ public class CrashClient : BaseClient
     public async Task<T> Request<T>(string api, IProtocol protocol)
         where T : class, IProtocol
     {
-        return await Request<T>(CrashResources.LoadServerSettings().lobbyServerAddress, api, protocol);
+        using (new LogScope($"Request<{typeof(T)}>, http, {api}"))
+            return await base.Request<T>(CrashResources.LoadServerSettings().lobbyServerAddress, api, protocol);
+    }
+
+    public new async Task<T> Request<T>(IProtocol protocol, TimeSpan? timeout = null) where T : class, IProtocol
+    {
+        using (new LogScope($"Request<{typeof(T)}>, socket, {timeout}"))
+            return await base.Request<T>(protocol, timeout);
+    }
+
+    public new async Task Send(IProtocol protocol)
+    {
+        using (new LogScope($"Send, {protocol.GetType()}"))
+            await base.Send(protocol);
     }
 }
